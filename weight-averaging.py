93b32eb2ad7cdd564d65e82bf6bf47e633c94d97
Inwerwm <saclainvervm@gmail.com>
2022-10-21 22:51:38 +0900

    オペレータプロパティと翻訳の実装

--- weight-averaging.py
+++ weight-averaging.py
@@ -1,4 +1,5 @@
 import bpy
+from bpy.props import EnumProperty
 
 bl_info = {
     "name": "Weight Averaging",
@@ -14,6 +15,29 @@ bl_info = {
     "category": "Object"
 }
 
+word_map = {
+    "en_US": {
+        ("*", "Average Weights"): "Averaging",
+        ("*", "Averages the weights of the selected vertices"): "Averages the weights of the selected vertices",
+        ("*", "Target Layers"): "Target Layers",
+        ("*", "Target Layers Selection"): "Target Layers Selection",
+        ("*", "All Layers"): "All Layers",
+        ("*", "Averaging all data layers"): "Averaging all data layers",
+        ("*", "Active Layer"): "Active Layer",
+        ("*", "Only averaging active data layer"): "Only averaging active data layer",
+    },
+    "ja_JP": {
+        ("*", "Average Weights"): "平均化",
+        ("*", "Averages the weights of the selected vertices"): "選択頂点のウェイトを平均化します",
+        ("*", "Target Layers"): "対象レイヤー",
+        ("*", "Target Layers Selection"): "複数のレイヤーがある場合、どのレイヤーを平均化するか",
+        ("*", "All Layers"): "全レイヤー",
+        ("*", "Averaging all data layers"): "全データレイヤーを平均化します",
+        ("*", "Active Layer"): "アクティブレイヤー",
+        ("*", "Only averaging active data layer"): "アクティブデータレイヤーのみ平均化します",
+    }
+}
+
 
 class INWERWM_OT_WeightAveraging(bpy.types.Operator):
     bl_idname: str = "object.weight_averaging"
@@ -21,13 +45,24 @@ class INWERWM_OT_WeightAveraging(bpy.types.Operator):
     bl_description: str = "Averages the weights of the selected vertices"
     bl_options = {'REGISTER', 'UNDO'}
 
+    target_layer: EnumProperty(
+        name="Target Layers",
+        description="Target Layers Selection",
+        default="All",
+        items=[
+            ("All", "All Layers", "Averaging all data layers"),
+            ("Active", "Active Layer", "Only averaging active data layer"),
+        ]  # type: ignore
+    )
+
     def execute(self, context: bpy.types.Context):
         return {'FINISHED'}
 
 
 def draw_menu(self, context):
     self.layout.separator()
-    self.layout.operator(INWERWM_OT_WeightAveraging.bl_idname)
+    self.layout.operator(INWERWM_OT_WeightAveraging.bl_idname,
+                         text=bpy.app.translations.pgettext("Average Weights"))
 
 
 # Blender に登録するクラス
@@ -40,9 +75,11 @@ def register():
     for c in classes:
         bpy.utils.register_class(c)
     bpy.types.VIEW3D_MT_edit_mesh_weights.append(draw_menu)
+    bpy.app.translations.register(__name__, word_map)
 
 
 def unregister():
+    bpy.app.translations.unregister(__name__)
     bpy.types.VIEW3D_MT_edit_mesh_weights.remove(draw_menu)
     for c in classes:
         bpy.utils.unregister_class(c)
