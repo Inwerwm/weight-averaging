d3ae7a154e8912b01be240a68ab69e1d24669a84
Inwerwm <saclainvervm@gmail.com>
2022-10-21 21:50:10 +0900

    メニュー登録と実行の定義を作成

--- weight-averaging.py
+++ weight-averaging.py
@@ -1,24 +1,51 @@
+import bpy
+
 bl_info = {
     "name": "Weight Averaging",
     "author": "Inwerwm",
     "version": (1, 0),
-    "blender": (3, 3, 1),
+    "blender": (3, 3, 0),
     "location": "",
     "description": "Averages the weights of the selected vertices.",
     "warning": "",
     "support": "TESTING",
     "doc_url": "",
     "tracker_url": "",
-    "category": "Object"
+    "category": "Rigging"
 }
 
 
+class INWERWM_OT_WeightAveraging(bpy.types.Operator):
+    bl_idname: str = "rigging.weight_averaging.inwerwm"
+    bl_label: str = "Average weights"
+    bl_description: str = "Averages the weights of the selected vertices."
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context: bpy.types.Context):
+        return {'FINISHED'}
+
+
+def draw_menu(self, context):
+    self.layout.separator()
+    self.layout.operator(INWERWM_OT_WeightAveraging.bl_idname)
+
+
+# Blender に登録するクラス
+classes = [
+    INWERWM_OT_WeightAveraging,
+]
+
+
 def register():
-    print("Enable Weight Averaging")
+    for c in classes:
+        bpy.utils.register_class(c)
+    bpy.types.VIEW3D_MT_mesh_add.append(draw_menu)
 
 
 def unregister():
-    print("Disable Weight Averaging")
+    bpy.types.VIEW3D_MT_mesh_add.remove(draw_menu)
+    for c in classes:
+        bpy.utils.unregister_class(c)
 
 
 if __name__ == "__main__":
